5ac57849abf6f88b11dfba1a9f77de320ce78709
Edwin Wise <Edwin.Wise@bazaarvoice.com>
2016-02-07 07:53:24 -0600

    First pass

---
+++ main.py
@@ -0,0 +1,134 @@
+
+import sys
+import string
+import math
+from datetime import date
+from gedcom import Gedcom
+
+
+def generate_entity_row(entity, level):
+    id = string.join(entity.name(), ' ')
+
+    if level == 1:
+        id = "    {}".format(id)
+    elif level == 2:
+        id = "    ... {}".format(id)
+
+    death_year = entity.death_year()
+    if death_year < 0:
+        final_year = date.today().year
+    else:
+        final_year = death_year
+
+    return {
+        'id': id,
+        'birth': entity.birth_year(),
+        'death': death_year,
+        'final': final_year
+    }
+
+def generate_fingerprint(row, id_length, earliest_census, latest_census):
+
+    if row is None:
+        return generate_fingerprint_title(id_length, earliest_census, latest_census)
+    else:
+        return generate_fingerprint_entry(row, id_length, earliest_census, latest_census)
+
+date_width = 6
+
+def generate_fingerprint_title(id_length, earliest_census, latest_census):
+
+    title = string.ljust('', id_length + date_width, ' ')
+
+    for date in range(earliest_census, latest_census+1, 10):
+        title += string.ljust(str(date), date_width, ' ')
+
+    return title
+
+def generate_fingerprint_entry(row, id_length, earliest_census, latest_census):
+
+    entry = string.ljust(row['id'], id_length, ' ')
+
+    birth = int(row['birth'])
+    final = int(row['final'])
+    if birth < 0:
+        entry += string.ljust('--', date_width, ' ')
+    else:
+        entry += string.ljust(str(row['birth']), date_width, ' ')
+
+        for date in range(earliest_census, latest_census+1, 10):
+            age = date - int(row['birth'])
+            if age < 0:
+                entry += string.ljust('', date_width, ' ')
+            elif date <= final:
+                entry += string.ljust(str(age), date_width, ' ')
+            else:
+                entry += string.ljust('', date_width, ' ')
+
+    return entry
+
+
+def fingerprint(gedcom, target):
+
+    rows = []
+
+    parents = gedcom.get_parents(target)
+    for parent in parents:
+        rows.append(generate_entity_row(parent, 0))
+
+    rows.append(generate_entity_row(target, 1))
+
+    families = gedcom.families(target)
+    for family in families:
+        peers = gedcom.get_family_members(family, "PARENTS")
+        children = gedcom.get_family_members(family, "CHIL")
+
+        for peer in peers:
+            if (peer != target):
+                rows.append(generate_entity_row(peer, 1))
+
+        for child in children:
+            rows.append(generate_entity_row(child, 2))
+
+    longest_id = 0
+    earliest_date = date.today().year + 1
+    latest_date = 0
+    for row in rows:
+        id = row['id']
+        longest_id = max(longest_id, len(id))
+        birth = int(row['birth'])
+        if birth > 0:
+            earliest_date = min(earliest_date, birth)
+        latest_date = max(latest_date, int(row['final']))
+    longest_id = int(math.ceil((longest_id+1) / 4.0) * 4)
+    earliest_census = int(math.ceil(earliest_date / 10.0) * 10)
+    latest_census = int(math.floor(latest_date / 10.0) * 10)
+
+    entries = []
+    entries.append( generate_fingerprint(None, longest_id, earliest_census, latest_census))
+    for row in rows:
+        entries.append( generate_fingerprint(row, longest_id, earliest_census, latest_census))
+
+    print
+    print string.upper("FINGERPRINT FOR {}".format(string.join(target.name(), ' ')))
+
+    for entry in entries:
+        print entry
+    print
+
+
+if __name__ == "__main__":
+
+    filename = sys.argv[1]
+    firstname = sys.argv[2]
+    lastname = sys.argv[3]
+
+    gedcom = Gedcom(filename)
+
+    criteria = "name={}:surname={}".format(firstname, lastname)
+
+    for element in gedcom.element_list():
+        if element.criteria_match(criteria):
+            fingerprint(gedcom, element)
+
+    pass
